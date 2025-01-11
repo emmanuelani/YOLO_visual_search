73248dc6f64186d70bc8264f4ed2122e6fe16285
Emmanuel Ani <emmanuelani234@gmail.com>
2025-01-11 13:04:55 +0100

    changed host from localhost to 0.0.0.0 and port to 7000

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7000, reload=True)
